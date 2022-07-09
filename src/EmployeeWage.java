a0bc35e766f04ca23ec93d6caee3c154887d358a
solanki <anujaj2222.gmail.com>
2022-07-09 19:14:23 +0530

    Using Switch Case And Calculating Employee Daily Wage.

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,22 +1,26 @@
 
 public class EmployeeWage {
+	public static final int Is_Full_Time=1,Is_Part_Time=2,Emp_Rate_Per_Hour=20;
 	
 	public static void main(String[] args) {
 		
-		int Is_Full_Time=1,Is_Part_Time=2,Emp_Rate_Per_Hour=20;
 		int empHrs=0, empWage=0;
 		
-		
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if(empCheck==Is_Full_Time) {
+		int empCheck=(int) (Math.floor(Math.random()*10)%3);
+		switch(empCheck)
+		{	
+		case Is_Full_Time :
 			empHrs=8;
-		}
-		else if(empCheck==Is_Part_Time) {
+			break;
+		
+		case Is_Part_Time :
 			empHrs=4;
-		}
-		else {
+			break;
+		
+		default :
 			empHrs=0;
 		}
+		
 		empWage= empHrs *Emp_Rate_Per_Hour;
 		
 		System.out.println("Employee Daily Wage Is : "+empWage);
