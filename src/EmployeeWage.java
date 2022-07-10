987a09c96b298ca8d689ebe0fc772c6a1fd2b6e3
solanki <anujaj2222.gmail.com>
2022-07-10 14:55:18 +0530

    Calculating Employee Wage Of Multipal Companies Using Array.

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,26 +1,31 @@
 
 public class EmployeeWage {
 	public static final int Is_Full_Time=1,Is_Part_Time=2;
-	private String company ;
-	private final int Emp_Rate_Per_Hour ;
-	private final int Num_Of_Working_Days;
-	private final int Max_Working_Hrs_Per_Month;
-	private  int totalEmpWage;
 	
+	private int NumOfCompany=0;
+	private CompanyEmpWage[] companyEmpWageArray;
 	
-	
-	public EmployeeWage(String company, int emp_Rate_Per_Hour, int num_Of_Working_Days, int max_Working_Hrs_Per_Month) {
-	
-		this.company = company;
-		this.Emp_Rate_Per_Hour = emp_Rate_Per_Hour;
-		this.Num_Of_Working_Days = num_Of_Working_Days;
-		this.Max_Working_Hrs_Per_Month = max_Working_Hrs_Per_Month;
+	public EmployeeWage () {
+		companyEmpWageArray=new CompanyEmpWage[5];
 	}
+	
+	private void addCompanyEmpWage(String company, int emp_Rate_Per_Hour, int num_Of_Working_Days, int max_Working_Hrs_Per_Month) {
+		companyEmpWageArray[NumOfCompany]=new CompanyEmpWage(company, emp_Rate_Per_Hour, num_Of_Working_Days, max_Working_Hrs_Per_Month, max_Working_Hrs_Per_Month);
+		NumOfCompany++;
+	}
+	
+	private void computeEmpWage() {
+		for (int i=0; i< NumOfCompany;i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+	
 
-	public void computeWage() {
+	private int computeWage(CompanyEmpWage companyEmpWage) {
 		int empHrs=0, empWage=0,totalWorkingDays=0,totalEmpHrs=0;
 		
-		while(totalEmpHrs<=Max_Working_Hrs_Per_Month && totalWorkingDays<Num_Of_Working_Days) {
+		while(totalEmpHrs<=companyEmpWage.Max_Working_Hrs_Per_Month && totalWorkingDays<companyEmpWage.Num_Of_Working_Days) {
 			totalWorkingDays++;
 		int empCheck=(int) (Math.floor(Math.random()*10)%3);
 		switch(empCheck)
@@ -37,26 +42,21 @@ public class EmployeeWage {
 			empHrs=0;
 		}
 		
-		empWage= empHrs *Emp_Rate_Per_Hour;
-		totalEmpWage +=empWage;
+		empWage= empHrs * companyEmpWage.Emp_Rate_Per_Hour;
+		companyEmpWage.totalEmpWage +=empWage;
 		totalEmpHrs +=empHrs;
 		
 		}
+		return empWage;
 	}
 	
 
-	@Override
-	public String toString() {
-		return "EmployeeWage Total Wage For " +company+" is : " + totalEmpWage ;
-	}
-
 	public static void main(String[] args) {
-		EmployeeWage relaince=new EmployeeWage("Relaince Industry", 20, 2, 10);
-		EmployeeWage adaniGroup=new EmployeeWage("Adani Group", 10, 4, 20);
-		relaince.computeWage();
-		System.out.println(relaince);
-		adaniGroup.computeWage();
-		System.out.println(adaniGroup);
+		EmployeeWage wage=new EmployeeWage();
+		wage.addCompanyEmpWage("Relaince Industry", 10, 4, 20);
+		wage.addCompanyEmpWage("Adani Group", 20, 2, 10);
+		wage.computeEmpWage();
+		
 
 		
 	}
